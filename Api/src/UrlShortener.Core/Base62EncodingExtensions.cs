dde1decc92e0806f41178f790b01c28b20a598ae
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2025-03-05 00:03:19 -0300

    Base62 Encoding

---
+++ Api/src/UrlShortener.Core/Base62EncodingExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace UrlShortener.Core;
+
+public static class Base62EncodingExtensions
+{
+    private const string Base62Characters = "0123456789" +
+                                             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+ 
+                                              "abcdefghijklmnopqrstuvwxyz";
+    public static string EncodeToBase62(this int number)
+    {
+        if (number == 0)
+        {
+            return "0";
+        }
+
+        var result = new Stack<char>();
+        while (number > 0)
+        {
+            result.Push(Base62Characters[number % 62]);
+            number /= 62;
+        }
+
+        return new string(result.ToArray());
+    }
+}
